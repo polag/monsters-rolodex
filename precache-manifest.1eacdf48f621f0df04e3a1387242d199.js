386a9960f64c3fdfd021616aa06ab9ae046c0428
Paula Goicoechea <paula.goicoechea@gmail.com>
2020-04-20 15:19:01 +0200

    Updates

---
+++ precache-manifest.1eacdf48f621f0df04e3a1387242d199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685b4ad5fc38b6ebd8eb42bc2717c387",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38401faeaa7a83dbcd32",
+    "url": "/monsters-rolodex/static/css/main.b703d2e1.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38401faeaa7a83dbcd32",
+    "url": "/monsters-rolodex/static/js/main.5cce0b00.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
